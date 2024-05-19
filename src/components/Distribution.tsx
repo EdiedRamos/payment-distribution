4a9942dc7f3d497bc25132c9f8a2ad26f55bb7c9
EdiedRamos <edieisa22@gmail.com>
2024-05-19 18:39:52 -0500

    chore: add payment information

--- src/components/Distribution.tsx
+++ src/components/Distribution.tsx
@@ -1,3 +1,4 @@
+import { FaPlus } from "react-icons/fa";
 import { useState } from "react";
 
 enum Type {
@@ -5,14 +6,43 @@ enum Type {
   Payment,
 }
 
-interface Distribution {
+interface PaymentInfo {
   id: string;
-  type: Type;
+  title: string;
+  quantity: number;
+  percentage: number;
+  dateToPay: string;
+  currency: string;
 }
 
+interface Payment {
+  id: string;
+  type: Type.Payment;
+  information: PaymentInfo;
+}
+
+interface Interval {
+  id: string;
+  type: Type.Interval;
+}
+
+type Distribution = (Payment | Interval)[];
+
+const INFORMATION_MOCK: Omit<PaymentInfo, "id"> = {
+  title: "Anticipo",
+  quantity: 182,
+  percentage: 100,
+  dateToPay: "22 Ene, 2022",
+  currency: "USD",
+};
+
 export const Distribution = () => {
-  const [distribution, setDistribution] = useState<Distribution[]>([
-    { id: crypto.randomUUID(), type: Type.Payment },
+  const [distribution, setDistribution] = useState<Distribution>([
+    {
+      id: crypto.randomUUID(),
+      type: Type.Payment,
+      information: { id: crypto.randomUUID(), ...INFORMATION_MOCK },
+    },
     { id: crypto.randomUUID(), type: Type.Interval },
   ]);
 
@@ -20,26 +50,42 @@ export const Distribution = () => {
     console.log({ actionId });
     setDistribution((prev) => [
       ...prev,
-      { id: crypto.randomUUID(), type: Type.Payment },
+      {
+        id: crypto.randomUUID(),
+        type: Type.Payment,
+        information: { id: crypto.randomUUID(), ...INFORMATION_MOCK },
+      },
       { id: crypto.randomUUID(), type: Type.Interval },
     ]);
   };
 
   return (
-    <div className="w-[500px]  overflow-auto">
-      <div className="relative inline-flex gap-10 items-center overflow-auto">
-        <div className="absolute border-2 w-full -z-10"></div>
+    <div className="w-full h-[250px] overflow-auto">
+      <div className="relative inline-flex gap-20 items-center ml-16">
+        <div className="absolute border-2 w-full -z-10 border-gray-200"></div>
         {distribution.map((data) => {
           if (data.type === Type.Interval) {
             return (
               <button
                 onClick={() => handleDistribution(data.id)}
-                className="w-[20px] h-[20px] bg-orange-400 rounded-[50%]"
-              ></button>
+                className="flex justify-center items-center w-[25px] h-[25px] bg-gray-200 rounded-[50%]"
+              >
+                <FaPlus size={12} className="text-orange-500" />
+              </button>
             );
           }
           return (
-            <button className="w-[40px] h-[40px] bg-blue-400 rounded-[50%]"></button>
+            <div className="relative flex flex-col items-center text-center">
+              <button className="w-[50px] h-[50px] bg-orange-500 rounded-[50%]"></button>
+              <div className="absolute border-2 rounded-md p-2 min-w-[150px] top-14">
+                <p className="font-bold">{data.information.title}</p>
+                <p className="text-sm">
+                  {data.information.quantity} {data.information.currency} (
+                  {data.information.percentage}%)
+                </p>
+                <p className="text-sm">{data.information.dateToPay}</p>
+              </div>
+            </div>
           );
         })}
       </div>
