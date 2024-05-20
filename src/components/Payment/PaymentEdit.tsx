c29f6e556b4dada9e46e0d9280036db41d184c1b
EdiedRamos <edieisa22@gmail.com>
2024-05-20 15:53:59 -0500

    chore: implement edit payment design

--- src/components/Payment/PaymentEdit.tsx
+++ src/components/Payment/PaymentEdit.tsx
@@ -1,3 +1,5 @@
+import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
+
 import { Payment } from "@/models";
 
 interface PaymentInfoProps {
@@ -7,14 +9,38 @@ interface PaymentInfoProps {
 export const PaymentEdit = ({ payment }: PaymentInfoProps) => {
   return (
     <div className="relative flex flex-col items-center text-center">
-      <button className="w-[50px] h-[50px] bg-blue-500 rounded-[50%]"></button>
-      <div className="absolute border-2 rounded-md p-2 min-w-[150px] top-14">
-        <p className="font-bold">{payment.information.title}</p>
-        <p className="text-sm">
-          {payment.information.quantity} {payment.information.currency.code} (
-          {payment.information.percentage}%)
-        </p>
-        <p className="text-sm">{payment.information.dateToPay}</p>
+      <div className="w-[50px] h-[50px] bg-blue-500 rounded-[50%]"></div>
+      <div className="absolute bg-white border-2 rounded-md p-2 min-w-[150px] top-14">
+        <form className="w-[150px]">
+          <input
+            className="border-2 p-1 rounded-md w-full font-bold"
+            type="text"
+            value={payment.information.title}
+            placeholder="Título"
+          />
+          <div className="flex justify-between border-2 rounded-md mt-1 p-1">
+            <p className="font-bold">{payment.information.quantity}</p>
+            <p className="font-bold text-gray-400">
+              {payment.information.currency.code}
+            </p>
+          </div>
+          <div className="flex mt-2 gap-4 justify-center items-center">
+            <button type="button" className="w-[30px] h-[30px] text-orange-400">
+              <CiCircleMinus className="w-full h-full" />
+            </button>
+            <p>{payment.information.percentage}</p>
+            <button type="button" className="w-[30px] h-[30px] text-orange-400">
+              <CiCirclePlus className="w-full h-full" />
+            </button>
+          </div>
+          <div>
+            <p className="font-medium text-gray-500">Vence</p>
+            <input
+              className="bg-inherit focus:outline-none focus:ring-2 focus:ring-orange-400 focus:rounded-sm p-1"
+              type="date"
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
