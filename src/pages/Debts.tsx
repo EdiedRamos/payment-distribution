95adf6cacbb9301e50d2354774734b1b1359375b
EdiedRamos <edieisa22@gmail.com>
2024-05-22 16:33:39 -0500

    style: update card color according to debt status

--- src/pages/Debts.tsx
+++ src/pages/Debts.tsx
@@ -43,7 +43,13 @@ export const Debts = () => {
         <div
           onClick={() => goToPayment(debt.id)}
           key={debt.id}
-          className="bg-red-500 flex flex-col p-2 gap-2 text-white rounded-xl shadow-lg hover:cursor-pointer hover:opacity-90 w-[95%] sm:w-[350px]"
+          className={`${
+            debt.isPaid
+              ? "bg-green-500"
+              : debt.paid > 0
+              ? "bg-orange-500"
+              : "bg-slate-500"
+          } flex flex-col p-2 gap-2 text-white rounded-xl shadow-lg hover:cursor-pointer hover:opacity-90 w-[95%] sm:w-[350px]`}
         >
           <p>Deuda: {index + 1}</p>
           <p>
