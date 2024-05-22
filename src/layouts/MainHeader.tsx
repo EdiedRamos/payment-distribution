3b442f511ea57e9337fbc59862187a2f4bb19358
EdiedRamos <edieisa22@gmail.com>
2024-05-22 15:52:56 -0500

    feat: implement main header design

--- src/layouts/MainHeader.tsx
+++ src/layouts/MainHeader.tsx
@@ -3,7 +3,11 @@ import { Outlet } from "react-router";
 export const MainHeader = () => {
   return (
     <>
-      <div>MainHeader</div>
+      <div className="bg-gray-50 border-b-2 flex justify-center">
+        <button className="text-white bg-orange-500 p-3 m-2 rounded-md hover:bg-orange-600">
+          Agregar Pago
+        </button>
+      </div>
       <Outlet />
     </>
   );
