4be9c5317e6f6aafbfaaaba87d1f0f4a711b02c6
EdiedRamos <edieisa22@gmail.com>
2024-05-23 14:28:41 -0500

    style: update edition styles

--- src/components/Payment/PaymentEdit.tsx
+++ src/components/Payment/PaymentEdit.tsx
@@ -20,7 +20,11 @@ export const PaymentEdit = ({ payment }: PaymentInfoProps) => {
 
   return (
     <div className="relative flex flex-col items-center text-center">
-      <div className="w-[50px] h-[50px] bg-blue-500 rounded-[50%]"></div>
+      <div
+        className={`${
+          payment.isPaid ? "bg-green-500" : "bg-blue-500"
+        } w-[50px] h-[50px] rounded-[50%]`}
+      ></div>
       <div
         className={`${
           validation.hasError ? "border-red-500" : ""
@@ -28,6 +32,7 @@ export const PaymentEdit = ({ payment }: PaymentInfoProps) => {
       >
         <form className="w-[150px]">
           <input
+            disabled={payment.isPaid}
             className="border-2 p-1 rounded-md w-full font-bold"
             type="text"
             onChange={handleChangeTitle}
@@ -40,35 +45,39 @@ export const PaymentEdit = ({ payment }: PaymentInfoProps) => {
               {payment.information.currency.code}
             </p>
           </div>
-          <div className="flex mt-2 gap-4 justify-center items-center">
-            <button
-              disabled={cantChangePercentage}
-              onClick={handleDecrement}
-              type="button"
-              className="w-[30px] h-[30px] text-orange-400"
-            >
-              <CiCircleMinus className="w-full h-full" />
-            </button>
-            <p className="font-medium">{payment.information.percentage}%</p>
-            <button
-              disabled={cantChangePercentage}
-              onClick={handleIncrement}
-              type="button"
-              className="w-[30px] h-[30px] text-orange-400"
-            >
-              <CiCirclePlus className="w-full h-full" />
-            </button>
-          </div>
-          <div>
-            <p className="font-medium text-gray-500">Vence</p>
-            <input
-              disabled={payment.isPaid}
-              onChange={handleChangeDate}
-              value={toYearMonthDay(payment.information.dateToPay)}
-              className="bg-inherit focus:outline-none focus:ring-2 focus:ring-orange-400 focus:rounded-sm p-1"
-              type="date"
-            />
-          </div>
+          {!payment.isPaid && (
+            <div className="flex mt-2 gap-4 justify-center items-center">
+              <button
+                disabled={cantChangePercentage}
+                onClick={handleDecrement}
+                type="button"
+                className="w-[30px] h-[30px] text-orange-400"
+              >
+                <CiCircleMinus className="w-full h-full" />
+              </button>
+              <p className="font-medium">{payment.information.percentage}%</p>
+              <button
+                disabled={cantChangePercentage}
+                onClick={handleIncrement}
+                type="button"
+                className="w-[30px] h-[30px] text-orange-400"
+              >
+                <CiCirclePlus className="w-full h-full" />
+              </button>
+            </div>
+          )}
+          {!payment.isPaid && (
+            <div>
+              <p className="font-medium text-gray-500">Vence</p>
+              <input
+                disabled={payment.isPaid}
+                onChange={handleChangeDate}
+                value={toYearMonthDay(payment.information.dateToPay)}
+                className="bg-inherit focus:outline-none focus:ring-2 focus:ring-orange-400 focus:rounded-sm p-1"
+                type="date"
+              />
+            </div>
+          )}
         </form>
         {validation.hasError && (
           <p className="text-red-500 font-medium">{validation.message}</p>
