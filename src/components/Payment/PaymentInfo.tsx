477fc31beb8bf10d8d1975a8496c2b2936cde8c2
EdiedRamos <edieisa22@gmail.com>
2024-05-23 15:15:53 -0500

    style: update payment styles

--- src/components/Payment/PaymentInfo.tsx
+++ src/components/Payment/PaymentInfo.tsx
@@ -1,5 +1,7 @@
 import { toAbbreviateDate, withOneDecimal } from "@/utils";
 
+import { GiTakeMyMoney } from "react-icons/gi";
+import { LuPartyPopper } from "react-icons/lu";
 import { Payment } from "@/models";
 import { usePayment } from "@/contexts";
 
@@ -11,7 +13,7 @@ export const PaymentInfo = ({ payment }: PaymentInfoProps) => {
   const { handlePayTransactionStart, nextToPayId } = usePayment();
 
   return (
-    <div className="relative flex flex-col items-center text-center">
+    <div className="relative flex flex-col items-center text-center font-medium">
       <button
         disabled={nextToPayId !== payment.id}
         onClick={() => handlePayTransactionStart(payment.id)}
@@ -22,8 +24,13 @@ export const PaymentInfo = ({ payment }: PaymentInfoProps) => {
             ? "bg-gray-200"
             : "bg-orange-500"
         }  rounded-[50%]`}
-      ></button>
-      <div className="absolute border-2 rounded-md p-2 min-w-[150px] top-14">
+      >
+        {payment.isPaid && <LuPartyPopper className="w-full h-6 text-white" />}
+        {payment.id === nextToPayId && (
+          <GiTakeMyMoney className="w-full h-6 text-white" />
+        )}
+      </button>
+      <div className="absolute bg-gray-100 border-2 rounded-md p-2 min-w-[150px] top-14">
         <p className="font-bold">{payment.information.title}</p>
         <p className="text-sm">
           {withOneDecimal(payment.information.quantity)}{" "}
@@ -31,7 +38,7 @@ export const PaymentInfo = ({ payment }: PaymentInfoProps) => {
           %)
         </p>
         {payment.isPaid && payment.transaction ? (
-          <p className="text-green-600">
+          <p className="bg-green-500 rounded-md p-1 text-white text-sm mt-1">
             Pagado {toAbbreviateDate(payment.transaction.date)} con{" "}
             {payment?.transaction?.method}
           </p>
